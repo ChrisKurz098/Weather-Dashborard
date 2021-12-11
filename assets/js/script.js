cd1c65649e35b44de1376f742d0c018bb9e60b3c
CKURZ <ckurz098@gmail.com>
2021-12-11 14:36:03 -0800

    Featuire get search query complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
 
 function main() {
-
+//----------------------------------------------------------INIT PAGE-----------------------------------------//
     const searchFormEl = document.querySelector(".searchForm");
     const searchInputEl = document.querySelector(".inputBox");
-    let searchHistory = ["0", "1", "2", "3", "4"];
+    const listEl = document.querySelector(".list")
+    let searchHistory = ["", "", "", "", ""];
 
     //check if any prev searchs
     if (!localStorage.getItem("searches")) {
@@ -12,17 +13,25 @@ function main() {
     }
     // load the data from localStorage
     searchHistory = JSON.parse(localStorage.getItem("searches"));
+    updateSearchList(searchHistory);
+//----------------------------------------------------------------------------------------------------------//
 
-    //-------------------------Get the value from the input--------------------------------//
+
+            //-------------------------Get the value from the input--------------------------------//
     function getQuery(event) {
         event.preventDefault();
-        let search = searchInputEl.value;
-        console.log(search);
-        //we will want to log the result after its resquest is successfull but for now we will just display it on click
-        logSearch(search);
+        let query = searchInputEl.value.trim();
+        //if there is anything in the search input
+        if (query) {
+            searchInputEl.value = "";
+            console.log("The query is: ", query);
+
+            //runs the fetch request
+            fetchSearch(query);
+        }
     }
 
-    //Logs the search into the prev search results
+             //--------------------Logs the search into the prev search results array---------------------//
     function logSearch(search) {
         //if the current query isnt in the past search history
         if (searchHistory.every((currentValue) => currentValue !== search)) {
@@ -35,12 +44,52 @@ function main() {
             searchHistory[0] = search;
             //save updated array to loaclStorage
             localStorage.setItem("searches", JSON.stringify(searchHistory));
+            updateSearchList(searchHistory);
+        }
+    }
+            //----------------------------Updates the list of prev searches dynamically---------------------//
+    function updateSearchList(array) {
+        //while there is a child element
+        while (listEl.firstChild) {
+            //remove the child element until none left
+            listEl.removeChild(listEl.firstChild)
+        }
+        //now reate new elements/ update list
+        for (let i = 0; i < array.length; i++) {
+            let listItemEl = document.createElement("li")
+            //if there is text in the array index, make it visible with css classes
+            if (array[i]) {
+                listItemEl.classList = "border hoverHighlight";
+            }
+            listItemEl.textContent = array[i];
+            listEl.append(listItemEl);
+
+        }
+
+    }
+            ///-----------------------if a prev search list is clicked run a fetch for that list items textContent-----------------//
+    function getPrevSearch(event) {
+        itemText = event.target.textContent.trim()
+        //if there is text, run a fetch for text
+        if (itemText) {
+            fetchSearch(itemText);
         }
     }
 
 
-    searchFormEl.addEventListener("submit", getQuery);
+///--------------------------------------------------------------FETCH STARTS HERE---------------------------------------------///
+    function fetchSearch(query) {
+        console.log("Fetch request for:", query);
 
+        
+        //we will want to log the result after its resquest is successfull but for now we will just display it on click
+        logSearch(query);
+    }
+
+    //----Event Listeners---//
+    searchFormEl.addEventListener("submit", getQuery);
+    listEl.addEventListener("click", getPrevSearch);
+    /////--------------------------------------------------------------END MAIN-----------------------------------------------------------------/////
 }
 
 main();
