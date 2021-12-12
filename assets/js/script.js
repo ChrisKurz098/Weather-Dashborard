59ee7459056e28281c589b2f12f74d0c764377cb
CKURZ <ckurz098@gmail.com>
2021-12-11 16:41:33 -0800

    Added fetch fucntionality and display of current stats

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ function main() {
     const searchFormEl = document.querySelector(".searchForm");
     const searchInputEl = document.querySelector(".inputBox");
     const listEl = document.querySelector(".list")
+    const currentListEl = document.querySelector("#currentList");
     let searchHistory = ["", "", "", "", ""];
 
     //check if any prev searchs
@@ -47,7 +48,19 @@ function main() {
             updateSearchList(searchHistory);
         }
     }
-    //----------------------------Updates the list of prev searches dynamically---------------------//
+
+    ///-----------------------if a prev search list is clicked run a fetch for that list items textContent-----------------//
+    function getPrevSearch(event) {
+        itemText = event.target.textContent.trim()
+        //if there is text, run a fetch for text
+        if (itemText) {
+            convertQuery(itemText);
+        }
+    }
+
+    ////------------------------------------------------------UPDATE HTML DOM-------------------------------------------------//
+
+    //------Updates the list of prev searches dynamically----------------//
     function updateSearchList(array) {
         //while there is a child element
         while (listEl.firstChild) {
@@ -63,43 +76,69 @@ function main() {
             }
             listItemEl.textContent = array[i];
             listEl.append(listItemEl);
-
-        }
-
-    }
-    ///-----------------------if a prev search list is clicked run a fetch for that list items textContent-----------------//
-    function getPrevSearch(event) {
-        itemText = event.target.textContent.trim()
-        //if there is text, run a fetch for text
-        if (itemText) {
-            convertQuery(itemText);
         }
     }
+    ///--------Update City Headder-------///
+    function updateCityEl(data) {
+        const cityDisplayEl = document.querySelector("#cityDisplay");
+        let cityName = data[0].name + ', ' + data[0].state + ' ' + data[0].country;
+        cityDisplayEl.textContent = cityName;
+        return;
+    }
+
+    //-----Update DOM to display current weather stats---//
+    function currentWeatherDisplay(data) {
+        //remove all list elements from parent
+        while (currentListEl.firstChild) {
+            //remove the child element until none left
+            currentListEl.removeChild(currentListEl.firstChild)
+        }
+
+        //now create new elements/ update list
+        let currentTempEl = document.createElement("li")
+        currentTempEl.textContent = "Temp: " + Math.round(data.current.temp) + "\u00B0F";
+
+        let currentWindEl = document.createElement("li")
+        currentWindEl.textContent = "Wind: " + data.current.wind_speed + " MPH";
+
+        let currentHumEl = document.createElement("li")
+        currentHumEl.textContent = "Humidity: " + data.current.humidity + " %";
+
+        let currentUviEl = document.createElement("li")
+        currentUviEl.textContent = "UVI Index: " + data.current.uvi;
+        //append all to list
+        currentListEl.append(currentTempEl);
+        currentListEl.append(currentWindEl);
+        currentListEl.append(currentHumEl);
+        currentListEl.append(currentUviEl);
+
+    }
 
 
-
-    ///--------------------------------------------------------------FETCH STARTS HERE---------------------------------------------///
+    ///--------------------------------------------------------------FETCH REQUEST FUNCTIONS---------------------------------------------///
     function convertQuery(query) {
         let apiURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + query + '&limit=1&appid=e1e7eafa5c756ed866504aaf6f3cb529'
         fetch(apiURL).then(function (response) {
             if (response.ok) {
-                console.log('response: ', response);
                 response.json().then(function (data) {
                     //Check if data has info
                     if (data.length) {
                         //save the query since it is valid
-                        let save = data[0].name + ',' + data[0].state + ',' + data[0].country;
-                        logSearch(save);
-                        console.log(data);
+                        let savedNameData = data[0].name + ',' + data[0].state + ',' + data[0].country;
+                        logSearch(savedNameData);
+                        updateCityEl(data);
                         console.log('Lon: ', data[0].lon);
                         console.log('Lat: ', data[0].lat);
+                        fetchWeather(data[0].lat, data[0].lon);
                     }
                     else {
                         console.log("Not a valid city name");
                     }
 
                 });
-                
+            }
+            else {
+                console.log("Error getting response");
             }
         });
 
@@ -107,10 +146,18 @@ function main() {
 
 
 
-    function fetchSearch(lat, lon) {
-        console.log("Fetch request for:", query);
-        apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=e1e7eafa5c756ed866504aaf6f3cb529"
-        //we will want to log the result after its resquest is successfull but for now we will just display it on click
+    function fetchWeather(lat, lon) {
+        console.log("Fetch request for:", lat, lon);
+        apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=e1e7eafa5c756ed866504aaf6f3cb529"
+
+        fetch(apiURL).then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    console.log("Weather Data: ", data);
+                    currentWeatherDisplay(data);
+                });
+            }
+        });
 
     }
 
